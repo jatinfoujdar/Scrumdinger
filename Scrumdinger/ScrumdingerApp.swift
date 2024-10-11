3aec3bcf234f6d3c3de1e1e95e3bc72cc6fa7127
Jatin Foujdar <jatin321001@gmail.com>
2024-10-11 23:25:34 +0530

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by jatin foujdar on 11/10/24.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Scrumdinger()
+        }
+    }
+}
