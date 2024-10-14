9f3050959dc86a6d0bc5ac1300a5e9f2a6dd10a3
Jatin Foujdar <jatin321001@gmail.com>
2024-10-14 21:08:08 +0530

    DetailView commit

--- Scrumdinger/View/DetailView.swift
+++ Scrumdinger/View/DetailView.swift
@@ -8,13 +8,18 @@
 import SwiftUI
 
 struct DetailView: View {
+    let scrum : DailyScrum
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+       
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
     }
 }
 
 struct DetailView_Previews: PreviewProvider {
     static var previews: some View {
-        DetailView()
+        NavigationStack{
+            DetailView(scrum: DailyScrum.sampleData[0])
+        }
     }
 }
