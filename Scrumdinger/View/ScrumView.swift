bc6230c5e9a704b2ba5dd60b593b1321b44fedeb
Jatin Foujdar <jatin321001@gmail.com>
2024-10-14 20:52:15 +0530

    setup navigation commit

--- Scrumdinger/View/ScrumView.swift
+++ Scrumdinger/View/ScrumView.swift
@@ -10,9 +10,20 @@ import SwiftUI
 struct ScrumView: View {
     var scrums : [DailyScrum]
     var body: some View {
-        List(scrums) { scrum in
-            CardView(scrum: scrum)
-                .listRowBackground(scrum.theme.mainColor)
+        NavigationStack{
+            List(scrums) { scrum in
+                NavigationLink(destination: Text(scrum.title)){
+                CardView(scrum: scrum)
+            }
+            .listRowBackground(scrum.theme.mainColor)
+        }
+        .navigationTitle("Daily Scrums")
+        .toolbar{
+            Button(action : {}){
+                Image(systemName: "plus")
+            }
+            .accessibilityLabel("New Scrum")
+           }
         }
     }
 }
